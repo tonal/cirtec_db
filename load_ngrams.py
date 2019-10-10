5ad9c376a56f67a7ed7e148dcc8e1565836c16b0
Alexandr N Zamaraev (aka tonal) <tonal@promsoft.ru>
2019-10-10 14:59:01 +0700

    Данные по н-граммам и запросы на них

---
+++ load_ngrams.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Загрузка топиков и топиков в контексты
+"""
+from collections import Counter
+from functools import partial
+import json
+from operator import itemgetter
+from pathlib import Path
+
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+NGRAM_TEMPL = '*-gram-result.json'
+NGRAM_DIR = 'l'
+
+MONGO_URI = 'mongodb://localhost:27017/'
+# MONGO_URI = 'mongodb://frigate:27017/'
+
+
+def main():
+  with MongoClient(MONGO_URI, compressors='snappy') as client:
+    mdb = client['cirtec']
+    col_name = 'n_gramms'
+    mdb.drop_collection(col_name)
+    col_gramms = mdb[col_name]
+    insert = col_gramms.insert_one
+    obj_type = 'lemmas'
+
+    cnt = 0
+    PREF_LEN = len('linked_papers_')
+    for fngram in Path(NGRAM_DIR).glob(NGRAM_TEMPL):
+      nka = int(fngram.stem.split('-', 1)[0])
+      # col_name = f'gramm_{nka}_{NGRAM_DIR}'
+      print(fngram, nka, obj_type)
+
+      ngrams = json.load(fngram.open(encoding='utf-8'))
+      for title, doc in ngrams.items():
+        all_cnt = doc['count']
+        # print(' ', title, all_cnt)
+        linked_papers = []
+        lp_cnt = 0
+        for k, cdoc in enumerate(doc['ids'], 1):
+          g_pub_id, gcnt = cdoc.popitem()
+          if not g_pub_id.startswith('linked_papers_'):
+            continue
+          pub_id, ref_num, start = g_pub_id[PREF_LEN:].rsplit('_', 2)
+          # print('  ', k, pub_id, ref_num, start, gcnt)
+          linked_papers.append(dict(cont_id=f'{pub_id}@{start}', cnt=gcnt))
+          lp_cnt += gcnt
+        if not linked_papers:
+          continue
+        ngram_doc = dict(
+          _id=f'{obj_type}_{nka}_{title}', title=title, nka=nka, type=obj_type,
+          count_all=all_cnt, count_in_linked_papers=lp_cnt,
+          linked_papers=linked_papers)
+        insert(ngram_doc)
+        cnt += 1
+        if cnt % 1000 == 0:
+          print(' ', cnt, ngram_doc)
+          # return
+      print(' ', cnt, ngram_doc)
+  print(cnt)
+
+
+if __name__ == '__main__':
+  main()
---
+++ select_ngramms.py
@@ -0,0 +1,102 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+from collections import Counter, defaultdict
+
+from pymongo import MongoClient
+
+
+MONGO_URI = 'mongodb://localhost:27017/'
+
+AUTHOR = 'Oates'
+
+
+def main():
+  client = MongoClient(MONGO_URI, compressors='snappy')
+  mdb = client['cirtec']
+
+  print_freq_ngramms_by_frags(mdb.n_gramms, 50, nka=2)
+  print()
+  print_author_ngramms_by_frags(mdb.contexts, AUTHOR)
+
+
+def print_freq_ngramms_by_frags(
+  n_gramms, freq:int=100, *, nka:int=None, ltype:str=None):
+  if nka and ltype:
+    pipeline = [{'$match': {'nka': nka, 'type': ltype}}, ]
+  elif nka:
+    pipeline = [{'$match': {'nka': nka}}, ]
+  elif ltype:
+    pipeline = [{'$match': {'type': ltype}}, ]
+  else:
+    pipeline = []
+  pipeline += [
+    {'$unwind': '$linked_papers'},
+    {'$group': {
+      '_id': '$title', 'count': {'$sum': '$linked_papers.cnt'},
+      'conts': {'$addToSet': '$linked_papers.cont_id'}}},
+    {'$match': {'count': {'$gte': freq}}}, # {'$project': {'count': false}},
+    {'$unwind': '$conts'},
+    {'$lookup': {
+      'from': 'contexts', 'localField': 'conts', 'foreignField': '_id',
+      'as': 'context'}},
+    {'$unwind': '$context'},
+    {'$match': {'context.frag_num': {'$gt': 0}}},
+    {'$group': {
+      '_id': '$context.frag_num', 'count': {'$sum': 1},
+      'n_gramms': {'$addToSet': '$_id'}}},
+    {'$sort': {'_id': 1}}]
+  for doc in n_gramms.aggregate(pipeline):
+    print('fragment:', doc['_id'], 'cnt:', doc['count'])
+    for t in sorted(doc['n_gramms']):
+      print(f'  "{t}"')
+
+
+def print_author_ngramms_by_frags(contexts, author:str):
+  ngramms = {}
+  topics_cnt = Counter()
+  coaut = defaultdict(Counter)
+  for i, doc in enumerate(
+    contexts.aggregate([
+      {'$match': {'cocit_authors': 'Oates'}},
+      {'$project': {
+        'exact': False, 'prefix': False, 'suffix': False, 'topics': False}},
+      {'$lookup': {
+        'from': 'n_gramms', 'localField': '_id',
+        'foreignField': 'linked_papers.cont_id', 'as': 'ngramms'}},
+      # {$unwind: '$ngramms'},
+      {'$project': {'ngramms.linked_papers': False}},
+    ]), 1
+  ):
+    # print(i, doc)
+    coauthors = frozenset(c for c in doc['cocit_authors'] if c != AUTHOR)
+    for ngramm in doc['ngramms']:
+      title = ngramm['title']
+      topics_cnt[title] += 1
+      if title not in ngramms:
+        ngramms[title] = ngramm
+
+      for ca in coauthors:
+        coaut[ca][title] += 1
+  print(f"'{AUTHOR}' co-cited with topics:")
+  sort_key = key = lambda kv: (-kv[1], kv[0])
+  for n, i in sorted(topics_cnt.items(), key=sort_key):
+    msg = (f'  {i} '
+           f'cnt: {ngramms[n]["count_in_linked_papers"]} '
+           f'"{n}"')
+    print(msg)
+  print('including by co-cited authors:')
+  for i, (co, cnts) in enumerate(
+    sorted(coaut.items(), key=lambda kv: (-sum(kv[1].values()), kv[0])), 1):
+    # print(i, co, cnts)
+    # print(
+    #   f"  {i} '{co}': "
+    #   f"{', '.join('in fragment %s repeats: %s' % (f, c) for f, c in cnts.items())}")
+    print(' ', i, f"'{co}'")
+    for t, c in sorted(cnts.items(), key=sort_key):
+      print('  ', c, f'"{t}"')
+
+
+if __name__ == '__main__':
+  main()
