0a91e30090a39d32e562932681c44733945dbdd7
Alexandr N Zamaraev (aka tonal) <tonal@promsoft.ru>
2019-11-21 15:09:27 +0700

    Задача № 24412: Загрузка классификации на позитивные/негативные цитаты.

--- util_text.py
+++ util_text.py
@@ -1,6 +1,10 @@
 # -*- codong: utf-8 -*-
+from dataclasses import dataclass, field
 from functools import partial
 import re
+from typing import Dict
+
+import pymorphy2
 
 re_drop_no_wrd = partial(re.compile(r'[^\w\d]+', re.I | re.U).sub, ' ')
 
@@ -12,3 +16,20 @@ re_link = partial(re.compile(r'\[[^]]+\]').sub, LINK_CHAR)
 re_link_first = partial(
   re.compile(r'^[^]]+\]\)?\.?').sub, LINK_FIRST_CHAR, count=1)
 re_link_last = partial(re.compile(r'\(?\[[^]]+$').sub, LINK_LAST_CHAR, count=1)
+
+
+@dataclass(eq=False)
+class Text2Seq:
+  wrd_norm:Dict[str, str] = field(default_factory=dict)
+  morph:pymorphy2.MorphAnalyzer = field(default_factory=pymorphy2.MorphAnalyzer)
+
+  def get_norm(self, w):
+    wrd_norm = self.wrd_norm
+    ow = (
+      wrd_norm.get(w) or
+      wrd_norm.setdefault(w, self.morph.parse(w)[0].normal_form))
+    return ow
+
+  def seq2text(self, text):
+    seq = ' '.join(map(self.get_norm, re_drop_no_wrd(text).split()))
+    return seq
