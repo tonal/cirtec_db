15ea2467244197c7789dff336a1dbf145d63560d
Alexandr N Zamaraev (aka tonal) <tonal@promsoft.ru>
2020-07-18 13:16:51 +0700

    SRT-58 Статистика и сравнение авторов: Доработал статистику.

--- models_dev/db_authors.py
+++ models_dev/db_authors.py
@@ -29,6 +29,14 @@ def get_publics(
         {'$group': {
           '_id': {'pub_author': field, 'ref_author': '$refs.authors'}}},
         {'$group': {'_id': '$_id.pub_author', 'cnt': {'$sum': 1}}}, ],
+      'conts': [
+        {'$lookup': {
+          'from': 'contexts', 'localField': '_id', 'foreignField': 'pubid',
+          'as': 'cont'}},
+        {'$unwind': '$cont'},
+        {'$group': {
+          '_id': {'pub_author': '$uni_authors', 'cont': '$cont._id'}}},
+        {'$group': {'_id': '$_id.pub_author', 'cnt': {'$sum': 1}}}, ],
       'ngramms': [
         {'$lookup': {
           'from': 'contexts', 'localField': '_id', 'foreignField': 'pubid',
@@ -43,30 +51,33 @@ def get_publics(
         get_ngramm_filter(ngpr, 'ngrm'),
         {'$group': {'_id': {'pub_author': field, 'ngrm': '$ngrm._id'}}},
         {'$group': {'_id': '$_id.pub_author', 'cnt': {'$sum': 1}}}, ],
-      'topics': [
-        {'$lookup': {
-          'from': 'contexts', 'localField': '_id', 'foreignField': 'pubid',
-          'as': 'cont'}},
-        {'$unwind': '$cont'},
-        {'$unwind': '$cont.topics'},
-        {'$match': {'cont.topics.probability': {'$gte': probability}}},
-        {'$group': {
-          '_id': {'pub_author': field, 'topic': '$cont.topics._id'}}},
-        {'$group': {'_id': '$_id.pub_author', 'cnt': {'$sum': 1}}}, ],
+      # 'topics': [
+      #   {'$lookup': {
+      #     'from': 'contexts', 'localField': '_id', 'foreignField': 'pubid',
+      #     'as': 'cont'}},
+      #   {'$unwind': '$cont'},
+      #   {'$unwind': '$cont.topics'},
+      #   {'$match': {'cont.topics.probability': {'$gte': probability}}},
+      #   {'$group': {
+      #     '_id': {'pub_author': field, 'topic': '$cont.topics._id'}}},
+      #   {'$group': {'_id': '$_id.pub_author', 'cnt': {'$sum': 1}}}, ],
     }},
     {'$unwind': '$publications'},
     {'$unwind': '$references'},
     {'$match': {'$expr': {'$eq': ['$publications._id', '$references._id']}}},
     {'$unwind': '$ref_authors'},
     {'$match': {'$expr': {'$eq': ['$publications._id', '$ref_authors._id']}}},
+    {'$unwind': '$conts'},
+    {'$match': {'$expr': {'$eq': ['$publications._id', '$conts._id']}}},
     {'$unwind': '$ngramms'},
     {'$match': {'$expr': {'$eq': ['$publications._id', '$ngramms._id']}}},
-    {'$unwind': '$topics'},
-    {'$match': {'$expr': {'$eq': ['$publications._id', '$topics._id']}}},
+    # {'$unwind': '$topics'},
+    # {'$match': {'$expr': {'$eq': ['$publications._id', '$topics._id']}}},
     {'$project': {
       'author': '$publications._id', 'cnt_pubs': '$publications.cnt',
       'cnt_refs': '$references.cnt', 'cnt_ref_auths': '$ref_authors.cnt',
-      'cnt_ngramms': '$ngramms.cnt', 'cnt_topics': '$topics.cnt'}},
+      'cnt_conts': '$conts.cnt', 'cnt_ngramms': '$ngramms.cnt',}},
+      # 'cnt_topics': '$topics.cnt'}},
     {'$sort': {'author': 1}},
   ]
   return pipeline
